162b838fd2597ff1553d2dc7fcd036b3f329315f
Lucas_real7898YT <blacklucas227@gmail.com>
2025-03-22 14:31:49 -0400

    Add files via upload

--- playtime-starter-kit addon/red-hand-upgrader/red_hand_upgrader.gd
+++ playtime-starter-kit addon/red-hand-upgrader/red_hand_upgrader.gd
@@ -6,23 +6,29 @@ extends Node3D
 @onready var label2 = $Label3D2
 @onready var label3 = $Label3D3
 @onready var label4 = $Label3D4
+@onready var timer = $Timer
+@onready var itemhand = $Mesh2
 
 func _ready():
+	itemhand.visible = false
 	red.visible = false
 	interaction.visible = true
 	label1.visible = true
 	label2.visible = false
 	label3.visible = false
 	label4.visible = false
+	$Mesh/BasicInteraction.visible = false
 
 
 func _on_keycard_reader_inserted():
 	$AnimationPlayer.play("keycard inserted")
 	label1.visible = false
 	label2.visible = true
+	$Mesh/BasicInteraction.visible = true
 
 
 func _on_basic_interaction_player_interacted():
+	timer.start()
 	$Scanning.play()
 	$AnimationPlayer.play("hand upgrader")
 	red.visible = true
@@ -31,3 +37,6 @@ func _on_basic_interaction_player_interacted():
 	label4.visible = true
 	label3.visible = true
 	label2.visible = false
+	
+func _on_timer_timeout():
+	itemhand.visible = true
