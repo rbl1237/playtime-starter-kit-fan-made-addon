fc175c9139c4fd7f91bddff7b29107783ce6ef08
Lucas_real7898YT <blacklucas227@gmail.com>
2025-03-24 15:28:46 -0400

    Add files via upload

--- playtime-starter-kit addon/customhands/yellow hand 2.0/gateyellow.gd
+++ playtime-starter-kit addon/customhands/yellow hand 2.0/gateyellow.gd
@@ -3,5 +3,6 @@ extends Node3D
 @onready var Hand = $yellow2/HandGrab
 
 func _on_hand_grab_grabbed(hand):
-	$AnimationPlayer.play("new_animation")
-	Hand.enabled = false
+	if hand and Grabpack.right_hand.current_hand_node.name == "YellowHand":
+		$AnimationPlayer.play("new_animation")
+		Hand.enabled = false
