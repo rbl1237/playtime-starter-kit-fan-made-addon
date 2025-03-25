a1cc76d36e2caa59aeea6911835fafa1abe0a6ae
Lucas_real7898YT <blacklucas227@gmail.com>
2025-03-24 21:30:52 -0400

    Add files via upload

---
+++ playtime-starter-kit addon/customhands/cyan-hand/wall.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@onready var A = $CSGBox3D49/CSGBox3D50
+@onready var B = $CSGBox3D49/CSGBox3D51
+@onready var C = $CSGBox3D49/CSGBox3D52
+@onready var D = $CSGBox3D49/CSGBox3D53
+@onready var handp = $CSGBox3D49/MeshInstance3D
+@onready var handp3 = $CSGBox3D49/MeshInstance3D2
+
+func _ready():
+	A.visible = false
+	B.visible = false
+	D.visible = false
+	C.visible = false
+	handp.visible = true
+	handp3.visible = true
+
+func _on_hand_grab_grabbed(hand):
+	if hand and Grabpack.right_hand.current_hand_node.name == "CyanHand":
+		A.visible = true
+		B.visible = true
+		D.visible = true
+		C.visible = true
+		handp.visible = false
+		handp3.visible = false
+		$HandGrab.enabled = false
